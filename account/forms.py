6d5f5290b4c64ba9041741cdb13bdb9d3f640d8d
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-29 17:23:04 +0300

    end for registration page

--- account/forms.py
+++ account/forms.py
@@ -7,3 +7,16 @@ class CreatUserForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['email', 'first_name', 'last_name', 'password1', 'password2', 'is_writer']
+        labels = {
+            'is_writer': 'Are You a Writer? ',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CreatUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({'placeholder': 'email'})
+        self.fields['first_name'].widget.attrs.update({'placeholder': 'FirstName'})
+        self.fields['last_name'].widget.attrs.update({'placeholder': 'LastName'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Enter Your Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Your Password'})
+        self.fields['is_writer'].widget.attrs.update({'label': 'are You a writer?'})
