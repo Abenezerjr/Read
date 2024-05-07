5937b9f15cbfdd8f9be3be2c3e0db8da8c367497
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-05-07 12:46:52 +0300

    render subscription plat form

--- client/urls.py
+++ client/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('client-dashboard', views.client_dashboard, name='client-dashboard')
+    path('client-dashboard', views.client_dashboard, name='client-dashboard'),
+    path('read-article/', views.read_article, name='read-article'),
+    path('subscription_locked/', views.subscription_locked, name='subscription_locked'),
 ]
--- client/views.py
+++ client/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from writer.models import Article
 from django.contrib.auth.decorators import login_required
+from .models import Subscription
+from django.core.exceptions import ValidationError
 
 
 # Create your views here.
@@ -8,3 +10,29 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def client_dashboard(request):
     return render(request, 'client/client-dashbored.html')
+
+
+@login_required(login_url='login')
+def read_article(request):
+    # articles=None
+    try:
+        subDetails = Subscription.objects.get(user=request.user, is_active=True)
+    except:
+        # articles = None
+        return redirect('subscription_locked')
+
+    current_subscription_plan = subDetails.subscription_plan
+
+    if current_subscription_plan == 'Standard':
+        articles = Article.objects.all().filter(is_premium=False)
+    elif current_subscription_plan == 'Premium':
+        articles = Article.objects.all()
+
+    context = {
+        'articles': articles
+    }
+    return render(request, 'client/read-article.html', context)
+
+
+def subscription_locked(request):
+    return render(request,'client/subscription-locked.html')
