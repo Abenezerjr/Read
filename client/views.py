9e7e767d058e765ce034ab68b52a6e7f4efc4aa0
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-05-07 11:44:50 +0300

    add subscription modle

--- client/views.py
+++ client/views.py
@@ -7,4 +7,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def client_dashboard(request):
-    return render(request,'client/client-dashbored.html')
+    return render(request, 'client/client-dashbored.html')
